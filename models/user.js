da257a67f1033e2e16b20ef5214f9ca148fa08ee
Jacob Moss <jacobmoss09@gmail.com>
2019-09-16 00:15:27 -0500

    sequelize validation in place

--- models/user.js
+++ models/user.js
@@ -2,31 +2,49 @@ module.exports = function(sequelize, DataTypes) {
   const User = sequelize.define("User", {
     firstname: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: {
+        msg: "Please enter your first name."
+      },
       validate: {
-        notEmpty: true
+        notEmpty: {
+          msg: "Please enter your first name."
+        }
       }
     },
     lastname: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: {
+        msg: "Please enter your last name."
+      },
       validate: {
-        notEmpty: true
+        notEmpty: {
+          msg: "Please enter your last name."
+        }
       }
     },
     username: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: {
+        msg: "Please enter a username."
+      },
       validate: {
-        notEmpty: true
+        notEmpty: {
+          msg: "Please enter a username."
+        }
       }
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: {
+        msg: "Please enter a password at least 10 characters long."
+      },
       validate: {
         notEmpty: true,
-        len: [10,100]
+        len: {
+          args: [10,100],
+          msg: "Please enter a password at least 10 characters long."
+        },
+        
       }
     }
   });
