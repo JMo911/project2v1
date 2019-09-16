681006afc109ae8061f39e9799a79e1aaac37010
Jacob Moss <jacobmoss09@gmail.com>
2019-09-16 00:32:14 -0500

    password is hashed before getting pushed to db

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,13 +1,15 @@
 var db = require("../models");
+const bcrypt = require('bcrypt');
 
 module.exports = function(app) {
-  app.post("/api/users", function(req, res) {
+  app.post("/api/users", async function(req, res) {
     //create new user
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
     db.User.create({
       firstname: req.body.firstname,
       lastname: req.body.lastname,
       username: req.body.username,
-      password: req.body.password
+      password: hashedPassword
     })
     .then(post => {
       res.json(post);
